151b529a685a1430d47919e7cf4077428c9809f7
nekoany <958340556@qq.com>
2023-09-17 06:55:08 +0800

    11

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .views import api_v1,api_v2
-from .extensions import init_exts
+#from .extensions import init_exts
 
 
 def creat_app():
@@ -21,7 +21,7 @@ def creat_app():
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
     # 初始化插件
-    init_exts(app=app)
+ #   init_exts(app=app)
 
     return app
 
